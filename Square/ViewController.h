50c61d7e6b11bea6736b372d0cde86d1ea452865
qianfeng <qianfeng@qianfengmatoMacBook.local>
2013-11-12 10:11:39 +0800

    Initial Commit

---
+++ Square/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Square
+//
+//  Created by qianfeng on 13-11-12.
+//  Copyright (c) 2013年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Square/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Square
+//
+//  Created by qianfeng on 13-11-12.
+//  Copyright (c) 2013年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
